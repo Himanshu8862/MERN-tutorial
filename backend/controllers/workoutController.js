0e19a202607632d21a7de9f6836bcc557bc9cf7b
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-13 18:06:25 +0530

    added workout controller

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,93 @@
+const Workout = require("../models/Workout")
+const mongoose = require("mongoose")
+
+// GET all workout
+const getWorkoutsCont = async (req, res) => {
+    try {
+        const workouts = await Workout.find().sort({ "createdAt": -1 })
+        res.status(200).json(workouts)
+    } catch {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// GET single workout
+const getWorkoutCont = async (req, res) => {
+    const id = req.params.id
+    try {
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+        const workout = await Workout.findById(id);
+        if (!workout) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+        res.status(200).json({ workout })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
+}
+
+// POST a new workout
+const createWorkoutCont = async (req, res) => {
+    const { title, load, reps } = req.body;
+    // console.log(workout)
+    try {
+        const workout = await Workout.create({
+            title, reps, load
+        })
+        res.status(200).json(workout)
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
+}
+
+// DELETE a workout
+const deleteWorkoutCont = async (req, res) => {
+    const id = req.params.id;
+    try {
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+        const workout = await Workout.findOneAndDelete({_id: id})
+        if (!workout) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+        res.status(200).json(workout)
+
+    } catch (error) {
+        return res.status.json({ error: error.message })
+    }
+}
+
+
+// UPDATE a workout
+const updateWorkoutCont = async(req, res) =>{
+    const id = req.params.id;
+    try {
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+        const workout = await Workout.findOneAndUpdate({_id: id},{...req.body})
+        if (!workout) {
+            return res.status(400).json({ error: "No such workout" })
+        }
+
+        res.status(200).json(workout)
+
+    } catch (error) {
+        return res.status.json({ error: error.message })
+    }
+}
+
+
+module.exports = {
+    createWorkoutCont,
+    getWorkoutsCont,
+    getWorkoutCont,
+    deleteWorkoutCont,
+    updateWorkoutCont
+}
--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,49 +1,27 @@
 const express = require("express")
 const workoutRouter = express.Router()
-const Workout = require("../models/Workout")
+const {
+    createWorkoutCont,
+    getWorkoutsCont,
+    getWorkoutCont,
+    deleteWorkoutCont,
+    updateWorkoutCont
+} = require("../controllers/workoutController")
+
 
 // GET all workouts
-workoutRouter.get("/", (req,res)=>{
-    res.json({
-        mssg: "GET all workouts"
-    })
-})
+workoutRouter.get("/", getWorkoutsCont)
 
 // GET single workout
-workoutRouter.get("/:id", (req,res)=>{
-    res.json({
-        mssg: "GET a single workout"
-    })
-})
+workoutRouter.get("/:id", getWorkoutCont)
 
 // POST a new workout
-workoutRouter.post("/", async(req,res)=>{
-    const {title, load, reps} = req.body;
-    // console.log(workout)
-    try {
-        const workout = await Workout.create({
-            title, reps, load
-        })
-        res.status(200).json(workout)
-
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-
-})
+workoutRouter.post("/", createWorkoutCont)
 
 // DELETE a workout
-workoutRouter.delete("/:id", (req,res)=>{
-    res.json({
-        mssg: "DELETE a workout"
-    })
-})
+workoutRouter.delete("/:id", deleteWorkoutCont)
 
 // UPDATE a workout
-workoutRouter.patch("/:id", (req,res)=>{
-    res.json({
-        mssg: "UPDATE a workout"
-    })
-})
+workoutRouter.patch("/:id", updateWorkoutCont)
 
 module.exports = workoutRouter
