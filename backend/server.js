8a45b9b76e65f7ff8700bebe10ce427f14d6816d
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-13 16:32:48 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express")
 require("dotenv").config()
 const workoutRouter = require("./routes/workout")
+const mongoose = require("mongoose")
 
 // express app
 const app = express();
 
 // middleware
 app.use(express.json()) // any request that comes in, it looks into the body of the request and passes it to the req object
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
@@ -15,5 +16,15 @@ app.use((req,res,next)=>{
 // routes
 app.use("/api/workouts", workoutRouter);
 
-// listen for requests
-app.listen(process.env.PORT, console.log(`Listening on port ${process.env.PORT}`))
+// connect to DB
+mongoose.connect(process.env.MONGO_URI,)
+    .then(() => {
+        console.log("Connected to db");
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on port ${process.env.PORT}`)
+        })
+
+    }).catch((err) => {
+        console.log(err)
+    })
