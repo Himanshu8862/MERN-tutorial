3a31576a466ba3f1f011e2cdf26d6708c9cf7639
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-15 21:07:37 +0530

    added user register, hashing, validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,14 @@ const userLoginCont = async (req, res) => {
 
 // register user
 const userRegisterCont = async (req, res) => {
-    res.json({ mssg: "user resgister" })
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.register(email, password)
+        res.status(200).json({ email, user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,6 +13,34 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// static resgister method
+userSchema.statics.register = async function(email, password) { // we can not use arrow function here, because we are using this keyword
+
+    // validation
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Enter a valid email")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password should be of minimum 8 characters, with atleast 1 lowercase, 1 uppercase, 1 number and 1 symbol")
+    }
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error("Email already exsists")
+    }
+
+    // generate hashed password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hashedPassword })
+    return user
+}
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
