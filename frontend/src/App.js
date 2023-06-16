5ec84854257302e63912ca5e20b83cddbcfb4bee
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-16 17:56:10 +0530

    Protecting react routes
    made logical redirects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={ user ? <Home /> : <Navigate to="/login" /> }/>
           </Routes>
           <Routes>
-            <Route path="/login" element={<Login />}/>
+            <Route path="/login" element={ !user ? <Login /> : <Navigate to="/" /> }/>
           </Routes>
           <Routes>
-            <Route path="/register" element={<Register />}/>
+            <Route path="/register" element={ !user ? <Register /> : <Navigate to="/" /> }/>
           </Routes>
         </div>
       </BrowserRouter>
