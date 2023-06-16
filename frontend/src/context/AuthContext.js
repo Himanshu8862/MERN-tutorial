cb96fe325d3366470c312c39909b4458e62df481
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-16 15:30:50 +0530

    setup initial authContext

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react"
+import { createContext, useReducer, useEffect } from "react"
 
 export const AuthContext = createContext();
 
@@ -22,6 +22,16 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    // for the first time that page is loaded, we check if the user is logged in by looking at the localStorage
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"))
+        if (user) {
+            dispatch({ type: "LOGIN", payload: user })
+        }
+
+    }, [])
+
 
     console.log("AuthContext state: ", state)
 
