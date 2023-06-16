c1c0b1b6ff2ce82d461ce86cb698bf756654156c
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-16 10:38:53 +0530

    added register and login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -10,6 +12,12 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />}/>
+          </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+          <Routes>
+            <Route path="/register" element={<Register />}/>
           </Routes>
         </div>
       </BrowserRouter>
